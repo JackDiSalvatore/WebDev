f5f794cedd12a86a65b464121a71f9a2ce9385ec
Unknown <jdisalvatore7@gmail.com>
2017-10-10 08:27:30 -0400

    added more css and jQuery js

--- Chpt-jQuery/TodoList/todo-list.js
+++ Chpt-jQuery/TodoList/todo-list.js
@@ -1,4 +1,10 @@
 
+/* toggleInput handler */
+$("#toggleInput").on("click", function() {
+    $("input[type='text']").slideToggle(500);
+});
+
+/* input handler*/
 $("input[type='text']").keypress(function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13') {
