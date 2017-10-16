27fe437b0b89a601d636971f891fa720d85adcc0
jackdisalvatore <jdisalvatore7@gmail.com>
2017-10-15 20:51:36 -0400

    fixing js code

--- Chpt-jQuery/TodoList/assets/js/todo-list.js
+++ Chpt-jQuery/TodoList/assets/js/todo-list.js
@@ -6,27 +6,33 @@ $("#toggleInput").on("click", function() {
 
 /* input handler*/
 $("input[type='text']").keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
+    var keycode = event.which;
     if(keycode == '13') {
-        console.log("You entered: " + $(this).val());
-        $("ol").append("<li> " + $(this).val() + " </li>");
+        var todoText = $(this).val();
+
+        console.log("You entered: " + todoText);
+        $("ol").append("<li><i class='fa fa-trash-o trashCan' aria-hidden='true'></i> " + todoText + "</li>");
 
         $(this, "textarea").val("");
     }
 });
 
-/* Check off todo by clicking */
-$("li").click(function(){
-    $(this).toggleClass("checkOffItem");
-});
-
 /* Delete item */
-$(".trashCan").click(function(event) {
+/* .click() only add event listener to existing element on page */
+/* .on("clicK") will apply event lister to existing elements on page
+    as well as any future elements dynamically added */
+/* This says when a li is cliked inside a ol, run this code*/
+$("ol").on("click", ".trashCan", function(event) {
     console.log("Deleted item");
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+/* Check off todo by clicking */
+$("ol").on("click", "li", function(){
+    $(this).toggleClass("checkOffItem");
 });
 
 /*$("li").on("mouseenter", function() {
