a1e9a374ad55428867b88270a365627ff4e65f43
JackDiSalvatore <jackp.disalvatore@gmail.com>
2026-03-03 18:08:44 -0600

    update example
    Signed-off-by: JackDiSalvatore <jackp.disalvatore@gmail.com>

--- socket-example/public/scripts.js
+++ socket-example/public/scripts.js
@@ -1,22 +1,34 @@
 // Running in the client browser
 
-// console.log("Hello World");
-// console.log(io);
-
 const EVENTS = {
     WELCOME: 'welcome',
-    THANK_YOU: 'thankYou'
+    THANK_YOU: 'thankYou',
+    NEW_CLIENT: 'newClient'
 }
 
 // Connect to server
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect(
+    "http://localhost:4000",
+    {
+        auth: {
+            secret: "secret-here"
+        },
+        query: {
+            meaningOfLife: 42
+        }
+    }
+);
 
 // on: listen to evetns
 // emit: sends events
 
 socket.on(EVENTS.WELCOME, data => {
-    console.log(data);
+    console.log(`Recieved event 'welcome': ${data}`);
 
     // handle 'welcome' call back from server
     socket.emit(EVENTS.THANK_YOU, [4,5,6]);
 })
+
+socket.on(EVENTS.NEW_CLIENT, data => {
+    console.log(`Recieved event 'newClient': ${data}`);
+});
--- socket-example/server.js
+++ socket-example/server.js
@@ -5,7 +5,9 @@ app.use(express.static("public"));
 const PORT = process.env.PORT || 4000;
 
 const EVENTS = {
-    WELCOME: 'welcome'
+    WELCOME: 'welcome',
+    THANK_YOU: 'thankYou',
+    NEW_CLIENT: 'newClient'
 }
 
 const server = app.listen(
@@ -14,20 +16,26 @@ const server = app.listen(
 );
 
 // const socketio = require("socket.io");
-// const io = socketio(server, {});    // add socket.io to express server!
+// const io = socketio(server, {});    
 
 const { Server } = require("socket.io");
+
+// construct socket.io with express server
 const io = new Server(server, {});
 
 // Listen for (javascript/node) events
 io.on("connect", socket => {
+    console.log(socket.handshake);
     console.log(socket.id, " connected to the server");
 
     // Send event to client that just connected
     socket.emit(EVENTS.WELCOME, [1,2,3]);
 
-    // Listen to thankYou event
-    socket.on('thankYou', d => {
-        console.log(`message from client: ${d}`);
+    // Sent event to all sockets connected to `io`
+    io.emit(EVENTS.NEW_CLIENT, socket.id);
+
+    // Listen to thankYou events
+    socket.on(EVENTS.THANK_YOU, data => {
+        console.log(`Recieved event 'thankYou' from client: ${data}`);
     });
 });
